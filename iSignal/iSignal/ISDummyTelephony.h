75049049aa7d6da3d486421d4407c19827268544
Patrick Deng <motodexter@gmail.com>
2011-08-23 21:03:33 +0800

    Fix the name "ISDummnyTelephony" to "ISDummyTelephony".

---
+++ iSignal/iSignal/ISDummyTelephony.h
@@ -0,0 +1,44 @@
+//
+//  ISDummnyTelephony.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-23.
+//  Copyright 2011年 CodeAnimal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Foundation/NSArray.h>
+
+#import "ISCallbackDelegate.h"
+
+#define CARRIER_CMCC 0;
+#define CARRIER_CUNI 1;
+
+#define CELLULAR_SIGNAL_STRENGTH_HIGHEST -50;
+#define CELLULAR_SIGNAL_STRENGTH_LOWEST -110;
+#define CELLULAR_SIGNAL_STRENGTH_LOSS 20;
+
+static NSString* STR_THREAD_SIGNALMONITOR = @"Thread_SignalMonitor";
+
+@interface ISDummyTelephony : NSObject <ISCallbackDelegate>
+{
+    id<ISCallbackDelegate> callbackDelegate;
+    NSThread *signalMonitor;
+    NSInteger signalStrength;
+    NSString *carrier;
+}
+
+@property (nonatomic, retain) id<ISCallbackDelegate> callbackDelegate; 
+@property (nonatomic, retain) NSThread *signalMonitor;
+@property (nonatomic, retain) NSString *carrier;
+@property (nonatomic) NSInteger signalStrength;
+
++(NSString*) randomCarrier;
++(NSInteger) randomSignalStrength;
++(NSArray*) getCarrierList;
+
+-(void) startToService;
+-(void) stopFromService;
+
+@end
---
+++ iSignal/iSignal/ISDummyTelephony.m
@@ -0,0 +1,114 @@
+//
+//  ISDummnyTelephony.m
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-23.
+//  Copyright 2011年 CodeAnimal. All rights reserved.
+//
+
+#import "ISDummyTelephony.h"
+
+#import "ISMathUtils.h"
+
+@implementation ISDummyTelephony
+
+// Manual Codes Begin
+
+@synthesize callbackDelegate;
+@synthesize signalMonitor;
+@synthesize signalStrength;
+@synthesize carrier;
+
++(NSArray*) getCarrierList
+{
+    NSArray* carrierArray = [NSArray arrayWithObjects:NSLocalizedString(@"STR_CMCC",nil), NSLocalizedString(@"STR_CUNI", nil), nil];
+    return carrierArray;
+}
+
++(NSString*) randomCarrier
+{
+    NSInteger min = CARRIER_CMCC;
+    NSInteger max = CARRIER_CUNI;
+    NSInteger arrayIndex = [ISMathUtils generateRandomNSInteger:min andMax:max];
+    NSArray* carrierArray = [ISDummyTelephony getCarrierList];
+    return [carrierArray objectAtIndex:arrayIndex];
+}
+
++(NSInteger) randomSignalStrength
+{
+    NSInteger loss = CELLULAR_SIGNAL_STRENGTH_LOSS;
+    NSInteger low = CELLULAR_SIGNAL_STRENGTH_LOWEST;
+    NSInteger high = CELLULAR_SIGNAL_STRENGTH_HIGHEST;
+    
+    return [ISMathUtils generateRandomNSInteger:(low - loss) andMax:high];
+}
+
+-(void) startToService
+{
+    
+}
+
+-(void) stopFromService
+{
+    
+}
+
+-(void) refreshCarrier
+{
+    self.carrier = [ISDummyTelephony randomCarrier];
+}
+
+-(void) refreshSignalStrength
+{
+    self.signalStrength = [ISDummyTelephony randomSignalStrength];
+    // Callback delegate to notify listener
+    [self.callbackDelegate messageCallback:[NSNumber numberWithInt:self.signalStrength]];
+}
+
+-(void) signalMonitorRun
+{
+    while (TRUE) 
+    {
+        [self refreshSignalStrength];
+        // Here current thread need to sleep for a small period
+        
+    }
+}
+
+-(void) initSignalMonitor
+{
+    signalMonitor = [[NSThread alloc] initWithTarget:self selector:@selector(updateSignalStrength) object:nil];
+    extern NSString* STR_THREAD_SIGNALMONITOR;
+    [signalMonitor setName:STR_THREAD_SIGNALMONITOR];
+//    [signalMonitor start];
+}
+
+-(void) messageCallback:(id)message
+{
+    
+}
+
+- (void)dealloc
+{
+    [signalMonitor release];
+    [callbackDelegate release];
+    [carrier release];
+    
+    [super dealloc];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) 
+    {
+        // Initialization code here.
+        [self initSignalMonitor];
+    }
+    
+    return self;
+}
+
+// Manual Codes End
+
+@end
