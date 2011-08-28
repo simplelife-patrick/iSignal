48eba95c15ead64eb43ec224fe1a5488ccb8a5f1
Patrick Deng <motodexter@gmail.com>
2011-08-28 23:24:19 +0800

    ISAppConfigs class is used to save all configurable attributes' values in config view.

---
+++ iSignal/iSignal/ISAppConfigs.h
@@ -0,0 +1,27 @@
+//
+//  ISAppConfigs.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-28.
+//  Copyright 2011年 CodeAnimal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define CONFIG_ALARM_RING @"cfg_alarm_ring"
+#define CONFIG_ALARM_VIBRATE @"cfg_alarm_vibrate"
+
+@interface ISAppConfigs : NSObject
+{
+
+}
+
++(void) initConfigs;
+
++(BOOL) isRingAlarmOn;
++(void) setRingAlarmOn:(BOOL) flag;
+
++(BOOL) isVibrateAlarmOn;
++(void) setVibrateAlarmOn:(BOOL) flag;
+
+@end
