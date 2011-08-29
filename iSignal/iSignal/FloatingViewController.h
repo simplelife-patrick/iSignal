d019f6615856c1fdd46149dc3664d41ae54f78f7
Patrick Deng <motodexter@gmail.com>
2011-08-29 19:43:44 +0800

    New class for floating view.

---
+++ iSignal/iSignal/FloatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  FloatingViewController.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-29.
+//  Copyright 2011年 CodeAnimal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FloatingViewController : UIViewController
+
+@end
