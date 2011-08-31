3a242d3db6daf070f565977579940eec5fa0f2a6
Patrick Deng <motodexter@gmail.com>
2011-08-31 18:24:05 +0800

    Rename class "GlobalSettings" to "CBEnvironmentUtils".

---
+++ iSignal/iSignal/CBEnvironmentUtils.h
@@ -0,0 +1,36 @@
+//
+//  CBEnvironmentUtils.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-22.
+//  Copyright 2011 CodeBeaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <TargetConditionals.h>
+
+#import "iSignalAppDelegate.h"
+
+#define SIMULATOR 0
+#define IPHONE 1
+
+#if TARGET_IPHONE_SIMULATOR
+    #define DEVICE SIMULATOR
+#elif TARGET_OS_IPHONE
+    #define DEVICE IPHONE
+#endif
+
+@interface CBEnvironmentUtils : NSObject
+{
+    
+}
+
+// Sample for singleton in Objective-C, unnecessary to use.
++(CBEnvironmentUtils *)singletonInstance;
+
++(BOOL) isBackgroundRunningEnabled;
+
++(id<UIApplicationDelegate>) getAppDelegate;
+
+@end
---
+++ iSignal/iSignal/CBEnvironmentUtils.m
@@ -0,0 +1,121 @@
+//
+//  CBEnvironmentUtils.m
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-22.
+//  Copyright 2011 CodeBeaver. All rights reserved.
+//
+
+#import "CBEnvironmentUtils.h"
+
+@interface CBEnvironmentUtils (privateMethods)
+
+-(void)realRelease;
+
+@end
+
+
+@implementation CBEnvironmentUtils
+
+// Manual Codes Begin
+
+static CBEnvironmentUtils* instance = nil;
+
++(CBEnvironmentUtils *)singletonInstance
+{
+    @synchronized(self)
+    {
+        if (nil == instance) 
+        {
+            instance = [[self alloc] init];
+            DLog(@"Singleton instance created: %@", instance);
+        }
+    }
+    
+    return instance;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    DLog(@"Singleton instance created or returned: %@", instance);
+    @synchronized(self) 
+    {
+        if (nil == instance) 
+        {
+            instance = [super allocWithZone:zone];
+            return instance;
+        }
+    }
+    return nil;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    DLog(@"Singleton instance returned: %@", instance);
+    return self;
+}
+
+- (id)retain
+{
+    DLog(@"Singleton instance returned: %@", instance);
+    return self;
+}
+
+- (unsigned)retainCount
+{
+    DLog(@"Singleton instance's retainCount is always 1");
+    return 1;
+}
+
+- (oneway void)release
+{
+    DLog(@"Singleton instance release nothing here.");
+}
+
+- (id)autorelease
+{
+    DLog(@"Singleton instance returned: %@", instance);
+    return self;
+}
+
+-(void)realRelease
+{
+    DLog(@"Singleton instance real release itself here: %@", instance);    
+    [super release];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) 
+    {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+-(void)dealloc
+{
+    [super dealloc];
+}
+
++(BOOL) isBackgroundRunningEnabled
+{
+    UIDevice* device = [UIDevice currentDevice];  
+    BOOL backgroundSupported = NO;  
+    if ([device respondsToSelector:@selector(isMultitaskingSupported)])  
+    {
+        backgroundSupported = device.multitaskingSupported;        
+    }
+    return backgroundSupported;
+}
+
++(id<UIApplicationDelegate>) getAppDelegate
+{
+    return [[UIApplication sharedApplication] delegate];
+}
+
+// Manual Codes End
+
+@end
