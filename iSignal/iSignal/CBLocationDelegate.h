304f0ee34ecaed74af96849aaa8946b7fe2d6127
Patrick Deng <motodexter@gmail.com>
2011-08-31 18:52:43 +0800

    Rename class "ISLocationDelegate" to "CBLocationDelegate".

---
+++ iSignal/iSignal/CBLocationDelegate.h
@@ -0,0 +1,49 @@
+//
+//  CBLocationDelegate.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-8-30.
+//  Copyright 2011 CodeBeaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <CoreLocation/CoreLocation.h>
+
+#import "CBModule.h"
+
+#define EVENT_AVAILABLE_TIME_DIFFERENCE 15.0
+
+#define REGION_RADIUS_DEFAULT 100.0
+
+@interface CBLocationDelegate : NSObject <CLLocationManagerDelegate, CBModule>
+{
+    CLLocationManager *locationManager;
+    CLLocation *lastLocation;
+    CLLocation *currentLocation;
+    CLLocationDegrees regionRadius;
+}
+
+@property (nonatomic, retain) CLLocationManager *locationManager;
+
+@property (nonatomic, retain) CLLocation *lastLocation;
+@property (nonatomic, retain) CLLocation *currentLocation;
+@property (nonatomic) CLLocationDegrees regionRadius;
+
++(BOOL) isLocationServiceEnabled;
+
++(BOOL) isRegionMonitoringAvailable;
++(BOOL) isRegionMonitoringEnabled;
+
+-(void) setDistanceFilter:(CLLocationDistance) distance;
+-(void) setAccuracy:(CLLocationAccuracy) accuracy;
+
+-(void) initLocationManagerIfNecessary;
+-(void) initLocationManagerIfNecessary:(CLLocationAccuracy) accuracy andDistance:(CLLocationDistance) distance;
+
+-(void) startStandardUpdate;
+-(void) startSignificantChangeUpdates;
+
+-(BOOL) registerRegionWithCurrentLocationAndCircularOverlay:(NSString*)identifier;
+
+@end
