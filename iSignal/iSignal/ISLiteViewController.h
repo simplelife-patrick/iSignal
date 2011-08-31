c123bf8a2d6e4c0e65e5769f36c930d06f476012
Patrick Deng <motodexter@gmail.com>
2011-08-31 18:03:44 +0800

    Organization is changed from "CodeAnimal" to "CodeBeaver".

--- iSignal/iSignal/ISLiteViewController.h
+++ iSignal/iSignal/ISLiteViewController.h
@@ -3,7 +3,7 @@
 //  iSignal
 //
 //  Created by Patrick Deng on 11-8-21.
-//  Copyright 2011年 CodeAnimal. All rights reserved.
+//  Copyright 2011 CodeBeaver. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
