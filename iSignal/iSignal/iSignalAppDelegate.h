23df22fe6d345f935efd38f2b5c5eb74ba643325
Patrick Deng <motodexter@gmail.com>
2011-09-01 16:02:09 +0800

    Codes format.

--- iSignal/iSignal/iSignalAppDelegate.h
+++ iSignal/iSignal/iSignalAppDelegate.h
@@ -14,7 +14,6 @@
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
-
 @property (nonatomic, retain) IBOutlet ISSplashViewController *splashViewController;
 
 @property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
