8b6733b37ee4280e21ad1c9edbdde30573ed1bba
Patrick Deng <motodexter@gmail.com>
2011-09-01 18:06:49 +0800

    Add records view related files.

---
+++ iSignal/ISRecordsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ISRecordsViewController.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-9-1.
+//  Copyright 2011年 CodeBeaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISRecordsViewController : UIViewController
+
+@end
--- iSignal/iSignal/ISSwitchViewController.m
+++ iSignal/iSignal/ISSwitchViewController.m
@@ -162,27 +162,6 @@
     }
 }
 
-// Manual Codes Ends
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-#pragma mark - View lifecycle
-
 - (void)viewDidLoad
 {
     // Do any additional setup after loading the view from its nib.
@@ -203,6 +182,27 @@
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
+
+// Manual Codes Ends
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
