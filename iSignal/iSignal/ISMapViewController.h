9d887b06cd16b80ab4924f4a51c2e01c448adce0
Patrick Deng <motodexter@gmail.com>
2011-09-01 16:17:35 +0800

    Add files about map view.

---
+++ iSignal/iSignal/ISMapViewController.h
@@ -0,0 +1,17 @@
+//
+//  ISMapViewController.h
+//  iSignal
+//
+//  Created by Patrick Deng on 11-9-1.
+//  Copyright 2011年 CodeBeaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <MapKit/MapKit.h>
+
+@interface ISMapViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet MKMapView *mapView;
+
+@end
---
+++ iSignal/iSignal/ISMapViewController.m
@@ -0,0 +1,65 @@
+//
+//  ISMapViewController.m
+//  iSignal
+//
+//  Created by Patrick Deng on 11-9-1.
+//  Copyright 2011年 CodeBeaver. All rights reserved.
+//
+
+#import "ISMapViewController.h"
+
+@implementation ISMapViewController
+
+// Manual Codes Begin
+
+@synthesize mapView;
+
+- (void)viewDidUnload
+{
+    [self setMapView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)dealloc 
+{
+    [self.mapView release];
+    [super dealloc];
+}
+
+// Manual Codes End
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+@end
