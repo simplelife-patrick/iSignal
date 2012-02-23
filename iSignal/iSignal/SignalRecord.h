d51e5bceb7ec9d10d35fc14cb0f3e30fdbb6275a
Patrick Deng <motodexter@gmail.com>
2012-02-23 19:18:40 +0800

    Add core data entity class: SignalRecord.

---
+++ iSignal/iSignal/SignalRecord.h
@@ -0,0 +1,23 @@
+//
+//  SignalRecord.h
+//  iSignal
+//
+//  Created by Patrick Deng on 12-2-20.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface SignalRecord : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * duration;
+@property (nonatomic, retain) NSNumber * isSync;
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSString * tag;
+@property (nonatomic, retain) NSDate * time;
+@property (nonatomic, retain) NSString * type;
+
+@end
---
+++ iSignal/iSignal/SignalRecord.m
@@ -0,0 +1,22 @@
+//
+//  SignalRecord.m
+//  iSignal
+//
+//  Created by Patrick Deng on 12-2-20.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#import "SignalRecord.h"
+
+
+@implementation SignalRecord
+
+@dynamic duration;
+@dynamic isSync;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic tag;
+@dynamic time;
+@dynamic type;
+
+@end
