ca0777d511a4d1db4bc7b451a9e4f609de78e2fa
Patrick Deng <motodexter@gmail.com>
2012-03-02 20:10:52 +0800

    App notifications definition file.

---
+++ iSignal/iSignal/ISNotificationDefinitions.h
@@ -0,0 +1,14 @@
+//
+//  ISNotificationDefinitions.h
+//  iSignal
+//
+//  Created by Patrick Deng on 12-3-2.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#ifndef iSignal_ISNotificationDefinitions_h
+#define iSignal_ISNotificationDefinitions_h
+
+#define NOTIFICATION_NO_SIGNAL @"NO_SIGNAL"
+
+#endif
