ac5da0e1c98104b1ebef373e0d62b3ed195b9ad1
Patrick Deng <motodexter@gmail.com>
2012-03-03 00:19:03 +0800

    Add CBNotificationManager class files.

---
+++ iSignal/iSignal/CBNotificationManager.h
@@ -0,0 +1,13 @@
+//
+//  CBNotificationManager.h
+//  iSignal
+//
+//  Created by Patrick Deng on 12-3-2.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBNotificationManager : NSObject
+
+@end
---
+++ iSignal/iSignal/CBNotificationManager.m
@@ -0,0 +1,28 @@
+//
+//  CBNotificationManager.m
+//  iSignal
+//
+//  Created by Patrick Deng on 12-3-2.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#import "CBNotificationManager.h"
+
+@implementation CBNotificationManager
+
+- (id)init
+{
+    self = [super init];
+    if (self) 
+    {
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
