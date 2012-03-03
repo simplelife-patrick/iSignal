e0083ebc41c69a3f6101b516863588e77a7b1e5b
Patrick Deng <motodexter@gmail.com>
2012-03-03 16:04:06 +0800

    New IBOutlet UILabel progressLabel.

--- iSignal/iSignal/ISSplashViewController.h
+++ iSignal/iSignal/ISSplashViewController.h
@@ -12,13 +12,18 @@
 
 #import "CBFetchedResultsControllerIdentifier.h"
 
+#define PROGRESS_STR_LOADING @"Loading..."
+
 @interface ISSplashViewController : UIViewController
 
 @property (nonatomic, retain) IBOutlet ISSwitchViewController *switchViewController;
-@property (nonatomic, retain) IBOutlet UIImageView *splashImageView;
+
+@property (nonatomic, retain) IBOutlet UILabel *progressLabel;
 
 @property (nonatomic, retain) NSTimer *timer;
 
 -(void) loadAnyNecessaryStuff;
+
+-(void) updateProgress:(NSString*) progress;
 
 @end
