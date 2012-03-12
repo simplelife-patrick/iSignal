fc1d6a0c733c1f9a370a6a33a27edec78b4823e4
Patrick Deng <motodexter@gmail.com>
2012-03-12 19:28:28 +0800

    Temporarily tab bar items forbid image.

--- iSignal/iSignal/ISHomeViewController.m
+++ iSignal/iSignal/ISHomeViewController.m
@@ -27,7 +27,7 @@
 
 - (void)initTabBarItem
 {
-    UIImage* itemImage = [UIImage imageNamed:@"home32.png"];
+    UIImage* itemImage = nil;//[UIImage imageNamed:@"home32.png"];
     UITabBarItem* theItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"STR_TAB_HOME", nil) image:itemImage tag:TABVIEW_INDEX_HOMEVIEW];
     self.tabBarItem = theItem;
     [theItem release];    
