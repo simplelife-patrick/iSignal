3253f1a6067b117d599288ebd65e2bb648e29c3a
Patrick Deng <motodexter@gmail.com>
2012-03-30 13:02:35 +0800

    Global error codes definition header file.

---
+++ iSignal/iSignal/CBErrorCodes.h
@@ -0,0 +1,15 @@
+//
+//  CBErrorCodes.h
+//  iSignal
+//
+//  Created by Patrick Deng on 12-3-30.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#ifndef iSignal_CBErrorCodes_h
+#define iSignal_CBErrorCodes_h
+
+#define CORE_DATA_READ_ERROR  -1000
+#define CORE_DATA_WRITE_ERROR -1001 
+
+#endif
