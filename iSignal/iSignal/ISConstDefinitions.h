9fc510499cd4c06a5d7ddaa4d67402fc23c5d1e4
Patrick Deng <motodexter@gmail.com>
2012-06-09 22:51:58 +0800

    Add ISConstDefinitions header file.

---
+++ iSignal/iSignal/ISConstDefinitions.h
@@ -0,0 +1,12 @@
+//
+//  ISConstDefinitions.h
+//  iSignal
+//
+//  Created by Patrick Deng on 12-6-9.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#ifndef iSignal_ISConstDefinitions_h
+#define iSignal_ISConstDefinitions_h
+
+#endif
