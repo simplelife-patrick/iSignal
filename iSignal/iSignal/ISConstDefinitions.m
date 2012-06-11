86afec15be40eae47903937c215de18e71610340
Patrick Deng <motodexter@gmail.com>
2012-06-11 19:01:57 +0800

    Add ISConstDefinitions.m file.

---
+++ iSignal/iSignal/ISConstDefinitions.m
@@ -0,0 +1,11 @@
+//
+//  ISConstDefinitions.m
+//  iSignal
+//
+//  Created by Patrick Deng on 12-6-11.
+//  Copyright (c) 2012年 CodeBeaver. All rights reserved.
+//
+
+#import "ISConstDefinitions.h"
+
+const NSUInteger kMonitorViewPlotDataSize = 100;
